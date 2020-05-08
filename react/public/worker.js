2829d654af11bedee87f88e19c71eba61024713f
Michel-Alexandre Belanger <michel-alexandre.belanger@tritondigital.com>
2020-05-07 23:15:44 -0400

    update

--- react/public/worker.js
+++ react/public/worker.js
@@ -2,7 +2,7 @@ console.log("Service Worker Loader...");
 
 self.addEventListener("push", (e) => {
   const data = e.data.json();
-  console.log("Push Received...");
+  console.log("Push Received... kiki");
   self.registration.showNotification(data.title, {
     body: "Notified by Me",
     icon: "http://image.ibb.co/frYOFd/tmlogo.png",
